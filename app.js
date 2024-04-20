6073d920c515711a7419c03984f6f6cc4422ec1d
KaranSharma0712 <karan.sharma40029021@gmail.com>
2024-04-20 18:27:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const connectDB = require('./server/config/db');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const app = express();
+const PORT = 1209 || process.env.PORT;
+const {isActiveRoute} = require('./server/helpers/routeHelpers');
+connectDB();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'))
+
+app.use(session({
+    secret:'keyboard cat',
+    resave: false,
+    saveUninitialized:true,
+    store:MongoStore.create({
+        mongoUrl : process.env.MONGODB_URI
+    }),
+    cookie:{maxAge:new Date(Date.now()+(3600000))}
+}))
+
+//built in middleware function
+app.use(express.static('public'));
+
+app.use(expressLayout);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
+
+app.use('/',require('./server/routes/main'));
+app.use('/',require('./server/routes/admin'));
+
+app.listen(PORT,()=>{
+    console.log(`App is listening on port ${PORT}`);
+})
